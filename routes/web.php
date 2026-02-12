ba96dfa4b41ea66b7e56960b291d16157d3d15d8
leo lai <lyping0526@gmail.com>
2026-02-12 14:58:39 +0800

    part 1 laravel route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get("/hello", function(){
+    return response("<h1>hello word</h1>",200)
+        ->header('content-Type','text/plain');    
+});
 
+Route::get("/posts/{id}",function($id){
+    //dd($id);
+    return response("Posts ".$id);
+})->where('id',"[0-9]+");
+
+
+Route::get("/search",function(Request $request){
+    dd($request->name,$request->age);
+});
 
