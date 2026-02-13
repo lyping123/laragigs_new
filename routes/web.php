854402609dd9c82e9ab8a74f1b8f743728dddb33
leo lai <lyping0526@gmail.com>
2026-02-13 16:23:15 +0800

    part 11.2 middleware

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[ListingController::class,'index']);
-Route::get('/listings/create',[ListingController::class,'create'])->name('listing.create');
-Route::post('/listings',[ListingController::class,'store'])->name('listing.store');
-Route::get('/listings/{id}',[ListingController::class,'edit'])->name('editform');
-Route::put('/listings/{id}',[ListingController::class,'update'])->name('listing.update');
+Route::get('/listings/create',[ListingController::class,'create'])->name('listing.create')->middleware('checkauth');
+Route::post('/listings',[ListingController::class,'store'])->name('listing.store')->middleware('checkauth');
+Route::get('/listings/{id}',[ListingController::class,'edit'])->name('editform')->middleware('checkauth');
+Route::put('/listings/{id}',[ListingController::class,'update'])->name('listing.update')->middleware('checkauth');
 Route::get('/listing/{id}',[ListingController::class,'show'])->name('listinglist');
-Route::delete('/listings/{id}',[ListingController::class,'destroy'])->name('listing.destroy');
+Route::delete('/listings/{id}',[ListingController::class,'destroy'])->name('listing.destroy')->middleware('checkauth');
 
 
 Route::get("/register",[UserController::class,'create'])->name('register.create');
